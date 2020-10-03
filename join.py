fd1a53fc4b8986c577d14e8296b2cc1f827c717e
vikranth22446 <rama22446@gmail.com>
2020-10-02 19:21:33 -0700

    Update

--- join.py
+++ join.py
@@ -1,78 +1,155 @@
 from math import ceil
 
+
 class SortHash:
-	def __init__(self, numPages, numBuffers):
-		self.numPages = numPages
-		self.numBuffers = numBuffers
+    def __init__(self, numPages, numBuffers):
+        self.numPages = numPages
+        self.numBuffers = numBuffers
 
-	def sort(self):
-		B = self.numBuffers
-		P = self.numPages
-		runs = [P // B, B, P % B]
-		i = 0
-		while runs[0] + (runs[2] > 0) > 1:
-			print("Pass {}: {} runs of {} pages, 1 run of {} pages".format(i, runs[0], runs[1], runs[2]))
-			runs = [runs[0] // (B - 1), runs[1] * (B - 1), runs[2] + (runs[0] % (B - 1)) * runs[1]]
-			i += 1
-		print("Pass {}: {} runs of {} pages, 1 run of {} pages".format(i, runs[0], runs[1], runs[2]))
-		ios = 2 * P * (i + 1)
-		print("Total IO:", ios)
-		return ios
+    def sort(self):
+        B = self.numBuffers
+        P = self.numPages
+        runs = [P // B, B, P % B]
+        i = 0
+        while runs[0] + (runs[2] > 0) > 1:
+            print("Pass {}: {} runs of {} pages, 1 run of {} pages".format(i, runs[0], runs[1], runs[2]))
+            runs = [runs[0] // (B - 1), runs[1] * (B - 1), runs[2] + (runs[0] % (B - 1)) * runs[1]]
+            i += 1
+        print("Pass {}: {} runs of {} pages, 1 run of {} pages".format(i, runs[0], runs[1], runs[2]))
+        ios = 2 * P * (i + 1)
+        print("Total IO:", ios)
+        return ios
 
-	def hash(self):
-		B = self.numBuffers
-		ios = P = self.numPages
-		parts = 1
-		i = 0
-		while P > B:
-			parts *= B - 1
-			P = ceil(P / (B - 1))
-			print("Pass {}: {} partitions of {} pages".format(i, parts, P))
-			ios += P * parts * 2
-			i += 1
-		ios += P * parts
-		print("Total IO:", ios)
-		return ios
+    def hash(self):
+        B = self.numBuffers
+        ios = P = self.numPages
+        parts = 1
+        i = 0
+        while P > B:
+            parts *= B - 1
+            P = ceil(P / (B - 1))
+            print("Pass {}: {} partitions of {} pages".format(i, parts, P))
+            ios += P * parts * 2
+            i += 1
+        ios += P * parts
+        print("Total IO:", ios)
+        return ios
 
 
 class Join:
-	def __init__(self, numPages1, numRecords1, numPages2, numRecords2, numBuffers=3):
-		self.p1 = numPages1
-		self.r1 = numRecords1
-		self.p2 = numPages2
-		self.r2 = numRecords2
-		self.b = numBuffers
+    def __init__(self, numPages1, numRecords1, numPages2, numRecords2, numBuffers=3):
+        self.p1 = numPages1
+        self.r1 = numRecords1
+        self.p2 = numPages2
+        self.r2 = numRecords2
+        self.b = numBuffers
 
-	def SNLJ(self):
-		print("Relation 1 as outer: {}", self.p1 + self.r1 * self.p2)
-		print("Relation 2 as outer: {}", self.p2 + self.r2 * self.p1)
+    def SNLJ(self):
+        print("Relation 1 as outer: {}".format(self.p1 + self.r1 * self.p2))
+        print("Relation 2 as outer: {}".format(self.p2 + self.r2 * self.p1))
 
-	def PNLJ(self):
-		print("Relation 1 as outer: {}", self.p1 + self.p1 * self.p2)
-		print("Relation 2 as outer: {}", self.p2 + self.p2 * self.p1)
+    def PNLJ(self):
+        print("Relation 1 as outer: {}".format(self.p1 + self.p1 * self.p2))
+        print("Relation 2 as outer: {}".format(self.p2 + self.p2 * self.p1))
 
-	def BNLJ(self):
-		print("Relation 1 as outer: {}", self.p1 + ceil(self.p1 / (self.b - 2)) * self.p2)
-		print("Relation 2 as outer: {}", self.p2 + ceil(self.p2 / (self.b - 2)) * self.p1)
+    def BNLJ(self):
+        print("Relation 1 as outer: {}".format(
+            self.p1 + ceil(self.p1 / (self.b - 2)) * self.p2))
+        print("Relation 2 as outer: {}".format(
+            self.p2 + ceil(self.p2 / (self.b - 2)) * self.p1))
 
-	def hash(self):
-		b, p1, p2 = self.b, self.p1, self.p2
-		parts = 1
-		ios = p1 + p2
-		i = 0
-		while p1 > b - 2 and p2 > b - 2:
-			parts *= b - 1
-			p1, p2 = ceil(p1 / (b - 1)), ceil(p2 / (b - 1))
-			print("Pass {}: {} partitions".format(i, parts))
-			print("  Relation 1: {} pages\n  Relation 2: {} pages".format(p1, p2))
-			ios += (p1 + p2) * parts * 2
-			i += 1
-		print("Total IO:", ios)
-		return ios
+    # Above work for all joins
+    # below only work for equijoins
 
-	def sort(self):
-		ios = SortHash(self.p1, self.b).sort() + SortHash(self.p2, self.b).sort()
-		print("Total IO:", ios + self.p1 + self.p2)
+    def hash(self):
+        b, p1, p2 = self.b, self.p1, self.p2
+        parts = 1
+        ios = p1 + p2
+        i = 0
+        while p1 > b - 2 and p2 > b - 2:
+            parts *= b - 1
+            p1, p2 = ceil(p1 / (b - 1.0)), ceil(p2 / (b - 1.0))
+
+            print("Pass {}: {} partitions".format(i, parts))
+            print("  Relation 1: {} pages\n  Relation 2: {} pages".format(p1, p2))
+            ios += (p1 + p2) * parts * 2
+            i += 1
+        print("Partitioning: ", ios - (p1 * parts + p2 * parts))
+        print("Build and Probe Phase: ", p1 * parts + p2 * parts)
+
+        print("Total IO:", ios, "Total Passes", i)
+        return ios
+
+    def sort(self, merging_phase_only=False):
+        if merging_phase_only:
+            print("Merging Cost:", self.p1 + self.p2)
+            return
+        ios = SortHash(self.p1, self.b).sort() + SortHash(self.p2, self.b).sort()
+        print("Total IO:", ios + self.p1 + self.p2)
+
+    def INLJ(self, num_pages_reach_data, clustered=True):
+        def inlj_helper(p1, r1, r2, relation="1"):
+            io = p1 + r1 * num_pages_reach_data
+            extra_unclusterd = 0
+            if not clustered:
+                extra_unclusterd = r2
+            io += extra_unclusterd
+            print(
+                "Cost to Join Relation {} as Outer: {} + {}*{} + {} = {}".format(relation, p1, num_pages_reach_data, r2,
+                                                                                 extra_unclusterd, io))
+
+        inlj_helper(self.p1, self.r1, self.r2)
+        inlj_helper(self.p2, self.r2, self.r1, relation="2")
+
+    def index_scan(self, height, num_leaf, num_record_pages, tuples_per_record, selectivity):
+        print("height: {}, leaf Cost: {}, records cost: {}".format(height, ceil(num_leaf * selectivity),
+                                                                   ceil(
+                                                                       num_record_pages * tuples_per_record * selectivity)))
+        print("Total Index Scan Cost",
+              height + ceil(num_leaf * selectivity) + ceil(num_record_pages * tuples_per_record * selectivity))
 
 
-Join(100, 100 * 100, 50, 50 * 50, 20).sort()
+if __name__ == "__main__":
+    # SortHash(12, 4).sort()
+    # Join(500, 500 * 50, 600, 600*50, numBuffers=12).BNLJ() # 30500
+    # Join(500, 500 * 50, 600, 600*50, numBuffers=12).hash() # 5742
+    Join(500, 500 * 50, 600, 600*50, numBuffers=50).hash() # 7700 IO
+
+    # Join(200, 0, 20, 0, numBuffers=8).sort()
+    # Join(60, 35 * 1200, 20, 100 * 50, numBuffers=6).hash()
+
+    # Join(80, 20000, 5, 1000, numBuffers=102).hash()
+    # Join(80, 20000, 25, 1000, numBuffers=102).sort()
+
+
+
+
+
+
+
+
+
+"""
+CREATE TABLE students (
+name VARCHAR, 
+sid INTEGER UNIQUE, 
+edx_id INTEGER UNIQUE PRIMARY KEY, 
+email VARCHAR UNIQUE);
+
+CREATE TABLE assignments (
+assignment_name VARCHAR UNIQUE, 
+profile VARCHAR, 
+assignment_group VARCHAR,
+ weight FLOAT, 
+ due_date TIMESTAMP, 
+ PRIMARY KEY(assignment_name,profile));
+ 
+CREATE TABLE results(
+submission_id INTEGER PRIMARY  KEY,
+edx_id INT  REFERENCES students(edx_id),
+assignment_name VARCHAR REFERENCES assignments(assignment_name) ,
+profile VARCHAR,
+submission_time TIMESTAMP ,
+score FLOAT 
+);
+"""
--- transaction.py
+++ transaction.py
@@ -1,35 +1,42 @@
-import networkx as nx
 import matplotlib.pyplot as plt
+import networkx as nx
+
 
 def depGraph(schedule, filename):
-	G = nx.DiGraph()
-	schedule = schedule.split()
-	G.add_nodes_from([op[0] for op in schedule])
-	for i in range(len(schedule)):
-		for j in range(i + 1, len(schedule)):
-			if schedule[i][2] == schedule[j][2] and (schedule[i][1] == 'W' or schedule[j][1] == 'W'):
-				G.add_edge(schedule[i][0], schedule[j][0])
-	nx.draw(G, with_labels=True)
-	plt.savefig(filename + ".png")
-	return nx.is_directed_acyclic_graph(G)
+    G = nx.DiGraph()
+    schedule = schedule.split()
+    G.add_nodes_from([op[0] for op in schedule])
+    for i in range(len(schedule)):
+        for j in range(i + 1, len(schedule)):
+            if schedule[i][2] == schedule[j][2] and (schedule[i][1] == 'W' or schedule[j][1] == 'W'):
+                G.add_edge(schedule[i][0], schedule[j][0])
+    nx.draw_networkx(G, with_labels=True, arrows=True)
+    if nx.is_directed_acyclic_graph(G):
+        print("topoligical sort", nx.topological_sort(G))
+    plt.savefig(filename + ".png")
+    return nx.is_directed_acyclic_graph(G)
 
 
 def waitGraph(schedule, filename):
-	G = nx.DiGraph()
-	schedule = schedule.split()
-	G.add_nodes_from([op[0] for op in schedule])
-	for i in range(len(schedule)):
-		for j in range(i + 1, len(schedule)):
-			if G.out_degree(schedule[j][0]) == 0\
-					and schedule[i][2] == schedule[j][2]\
-					and (schedule[i][1] == 'X' or schedule[j][1] == 'X'):
-				G.add_edge(schedule[j][0], schedule[i][0])
-	nx.draw(G, with_labels=True)
-	plt.savefig(filename + ".png")
-	return nx.is_directed_acyclic_graph(G)
+    G = nx.DiGraph()
+    schedule = schedule.split()
+    G.add_nodes_from([op[0] for op in schedule])
+    for i in range(len(schedule)):
+        for j in range(i + 1, len(schedule)):
+            if G.out_degree(schedule[j][0]) == 0 \
+                    and schedule[i][2] == schedule[j][2] \
+                    and (schedule[i][1] == 'X' or schedule[j][1] == 'X'):
+                G.add_edge(schedule[j][0], schedule[i][0])
+                print(schedule[j][0], schedule[i][0])
+    nx.draw(G, with_labels=True, arrowstyle='->')
+    plt.savefig(filename + ".png")
+    return nx.is_directed_acyclic_graph(G)
 
 
-# print(depGraph("3RC 1RA 1WA 1RB 2WB 2RC 2WC 2WA 3WD", '1'))
-# depGraph("1RA 2RA 1RB 2RB 3RA 4RB 1WA 2WB", '2')
+if __name__ == "__main__":
+    # print(waitGraph("1SA 3SA 2XB 4SB 1XC 1SY 2XC 3XB", "1"))
+    # print(depGraph("1RB 2RB 3WB 4WB 2RD 3RC 1RC 4WC 4WD 1RA 2WA", "1"))
+    # print(depGraph("3RB 2WC 4WA 4WB 2RC 3WB 2RA 2RB 1RA 3RC", '1'))
+    # depGraph("1RA 2RA 1RB 2RB 3RA 4RB 1WA 2WB", '2')
 
-print(waitGraph("1SA 1SD 2XB 1SB 3SD 3SC 2XC 4XB 3XA", '3'))
+    print(waitGraph("3XE 2SE 1XB 1XA 4XA 3SB 2XD 1SD 4SD", '3'))
